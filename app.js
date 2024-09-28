379be1296b74af4addf4b992bdb63fdf2c538165
MariaChmielinska <maria.anna.chmielinska@gmail.com>
2024-09-28 13:43:44 +0200

    book models

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const flash = require('connect-flash');
+
+const app = express();
 
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 dotenv.config();
 
+const bookRoutes = require('./routes/book.routes');
+
+// Next() es el tercer parámetro que se puede definir en las funciones de callback de los métodos . get, post, etc.
+// voy a implementar manualmente un mini-morgan
+app.use((req, res, next) => {
+    console.log(`${new Date().toISOString()} Me acaban de hacer una petición a ${req.url}`);
+    next();
+});
+
+app.use('/api', bookRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+
+
+
+app.listen(PORT, () => {
+    // Nos conectamos a la base de datos de MongoDB
+    connectDB();
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+})
